432fb11b52f29ff38dfd62838d9d5a4cb414e491
unknown <omitogunjesufemi@gmail.com>
2022-07-01 15:58:03 +0100

    Webpage with list of Customers grouped by Country

---
+++ NorthwindCustomers/Areas/PacktFeatures/Pages/customers.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PacktFeatures.Pages
+{
+    public class CustomersPageModel : PageModel
+    {
+        private Northwind db;
+        public CustomersPageModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        
+        public IEnumerable<IGrouping<string, Customer>> Customers {get; set;}
+        public void OnGet()
+        {
+            Customers = db.Customers.AsEnumerable().GroupBy(cus=>cus.Country).OrderBy(cus=>cus.Key);
+        }
+    }
+
+}
+
