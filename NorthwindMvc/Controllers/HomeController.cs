71cf8539683ec21e9ef3f168e1476ea11ce8d6d6
unknown <omitogunjesufemi@gmail.com>
2022-07-06 09:12:33 +0100

    Using Display templates from Helper methods

--- NorthwindMvc/Controllers/HomeController.cs
+++ NorthwindMvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using NorthwindMvc.Models;
 using System;
 using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindMvc.Controllers;
 
@@ -72,4 +73,28 @@ public class HomeController : Controller
 
       return View(model);
     }
+
+    public IActionResult ProductsThatCostMoreThan(decimal? price)
+    {
+        if (!price.HasValue)
+        {
+            return NotFound("You must pass a product price in the query string");
+        }
+
+        IEnumerable<Product> model = db.Products
+        .Include(p=>p.Category)
+        .Include(p=>p.Supplier)
+        .AsEnumerable()
+        .Where(p=>p.UnitPrice > price);
+
+        if (model.Count() == 0)
+        {
+            return NotFound($"No product cost more than {price:C}");
+        }
+
+        ViewData["MaxPrice"] = price.Value.ToString("C");
+
+        return View(model);
+    }
+    
 }
